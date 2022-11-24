97fdec3fae3d35c9446f5786fe06688410bbd279
Samy <2031008@cegeprdl.ca>
2022-11-24 12:35:12 -0500

    oled showing temperature

--- src/main.cpp
+++ src/main.cpp
@@ -11,18 +11,18 @@ using namespace std;
 #include "MyOled.h"
 #include "MyServer.h"
 
+// variables et constantes pour led
+#define GPIOLedRed 27   // GPIO utilisee par la led rouge
+#define GPIOLedYellow 14   // GPIO utilisee par la led jaune
+#define GPIOLedGreen 12   // GPIO utilisee par la led verte
+bool ledOn = false; // pour garder en memoire l'etat de la led
 
-float tempAct=0;
 // variables et constantes pour dht22
-const unsigned int DHTPIN = 15; // GPIO utilisee par le DTH
+float tempAct=0; //pour la temperature actuelle
+char strTemperature[64] = ""; //pour convertir la temperature en char[] et l'afficher sur le oled
+const unsigned int GPIODHT = 15; // GPIO utilisee par le DTH
 #define DHTTYPE DHT22           // Modele du DHT
 MyDHT *dht;
-
-// variables et constantes pour led
-#define LedRed 27   // GPIO utilisee par la led rouge
-#define LedYellow 14   // GPIO utilisee par la led jaune
-#define LedGreen 12   // GPIO utilisee par la led verte
-bool ledOn = false; // pour garder en memoire l'etat de la led
 
 // Oled
 MyOled *myOled = NULL;
@@ -40,15 +40,15 @@ WiFiManager wm;
 void setup() {
   Serial.begin(115200);
   // initialisation de la pin de la led et s'assurer qu'elle est fermee au demarrage
-  pinMode(LedRed, OUTPUT);
-  pinMode(LedYellow, OUTPUT);
-  pinMode(LedGreen, OUTPUT);
-  digitalWrite(LedRed, LOW);
-  digitalWrite(LedYellow, LOW);
-  digitalWrite(LedGreen, LOW);
+  pinMode(GPIOLedRed, OUTPUT);
+  pinMode(GPIOLedYellow, OUTPUT);
+  pinMode(GPIOLedGreen, OUTPUT);
+  digitalWrite(GPIOLedRed, LOW);
+  digitalWrite(GPIOLedYellow, LOW);
+  digitalWrite(GPIOLedGreen, LOW);
 
   // initialisation de l'objet senseur de temperature
-  dht = new MyDHT(DHTPIN, DHTTYPE);
+  dht = new MyDHT(GPIODHT, DHTTYPE);
 
   myOled = new MyOled(&Wire, OLED_RESET, SCREEN_HEIGHT, SCREEN_WIDTH);
   myOled->init(100);
@@ -67,6 +67,13 @@ void setup() {
 void loop() {
   //tempAct = dht->getTemp(); // obtenir la temperature et la stocker dans la variable temp
   //dht->printTemp();      // afficher la temperature dans la console
+  
+  sprintf(strTemperature, "%g C", tempAct);
+  myOled->clearDisplay();
+  myOled->printIt(1, 2, "Patate: ",true);
+  myOled->printIt(15, 2, "Chepo",true);
+  myOled->printIt(8, 36, "Temp: ",true);
+  myOled->printIt(40, 36, strTemperature,true);
 
-  //delay(2000);
+  //delay(2000);(rduct)
 }
